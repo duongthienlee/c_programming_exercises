1ecfa2ad8444973ee2e92a75f5263f410d4ffa11
Thien Ly <thien.ly@aalto.fi>
2021-02-05 19:08:06 +0200

    Finish M02 exercises
    Signed-off-by: Thien Ly <thien.ly@aalto.fi>

--- M02/calc/src/source.c
+++ M02/calc/src/source.c
@@ -5,15 +5,39 @@
 
 void simple_multiply(void)
 {
-    
-}
+    int a, b;
 
+    scanf("%d", &a);
+    scanf("%d+\n", &b);
+
+    printf("%d * %d = %d\n", a, b, a * b);
+}
 
 void simple_math(void)
 {
-    
+    float a, b;
+    char operator;
+
+    scanf("%f", &a);
+    scanf(" %c ", &operator);
+    scanf("%f+\n", &b);
+
+    switch (operator)
+    {
+    case '+':
+        printf("%.1f\n", a + b);
+        break;
+    case '-':
+        printf("%.1f\n", a - b);
+        break;
+    case '*':
+        printf("%.1f\n", a * b);
+        break;
+    case '/':
+        printf("%.1f\n", a / b);
+        break;
+    default:
+        printf("ERR\n");
+        break;
+    }
 }
-
-
-
-
--- M02/triangle/src/source.c
+++ M02/triangle/src/source.c
@@ -4,5 +4,17 @@
 
 void draw_triangle(unsigned int size)
 {
-    
+    char sharp = '#';
+    char dot = '.';
+
+    scanf("%u", &size);
+
+    for (int o = 1; o <= size; o++)
+    {
+        for (int i = 0; i < size - o; i++, printf("%c", dot))
+            ;
+        for (int n = 0; n < o; n++, printf("%c", sharp))
+            ;
+        printf("\n");
+    }
 }
