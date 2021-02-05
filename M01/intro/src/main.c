7ebf7e9dcd0a3d3902fa1b3d51c8f44aad236f38
Thien Ly <thien.ly@aalto.fi>
2021-02-05 14:23:07 +0200

    Finish M01 exercises
    Signed-off-by: Thien Ly <thien.ly@aalto.fi>

--- M01/intro/src/main.c
+++ M01/intro/src/main.c
@@ -1,1 +1,8 @@
 #include <stdio.h>
+
+int main(void)
+{
+    printf("January\nFebruary\nMarch\n");
+
+    return 0;
+}
--- M01/vector/src/source.c
+++ M01/vector/src/source.c
@@ -1,6 +1,8 @@
 #include <math.h>
 #include <stdio.h>
 
-
-
-
+double vectorlength(double x, double y, double z)
+{
+    double powVal = (double)pow(x, 2) + (double)pow(y, 2) + (double)pow(z, 2);
+    return (double)sqrt(powVal);
+}
