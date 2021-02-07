d3ca0f846a5dd3bd51f9f5a2e93ec5786fe9eb6e
Thien Ly <duongthienlee@gmail.com>
2021-02-07 12:33:34 +0200

    Finish the last exercise of module 2.
    ascii
    
    Signed-off-by: Thien Ly <duongthienlee@gmail.com>

--- M02/ascii/src/source.c
+++ M02/ascii/src/source.c
@@ -6,5 +6,23 @@
 
 void ascii_chart(char min, char max)
 {
-    
+    for (int i = min; i <= max; i++)
+    {
+        /* three-character field that shows the given number as an integer. If the number
+         * takes less than three characters (it is < 100), it is aligned right.
+         * %3d Denotes field length/width.
+         */
+        printf("%3d 0x%02x", i, i);
+        // check if character is printable with isprint
+        if (isprint(i) == 0)
+            printf(" ?");
+        else
+            printf(" %c", i);
+
+        // break line at the fourth column
+        if ((i - min + 1) % 4 == 0)
+            printf("\n");
+        else
+            printf("\t");
+    }
 }
