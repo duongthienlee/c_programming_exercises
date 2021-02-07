4d1b2f20308c1b30b9b7128484a10877dcc718dd
Thien Ly <duongthienlee@gmail.com>
2021-02-07 10:37:47 +0200

    M02 Formatting triangle code.
    Signed-off-by: Thien Ly <duongthienlee@gmail.com>

--- M02/triangle/src/source.c
+++ M02/triangle/src/source.c
@@ -11,10 +11,15 @@ void draw_triangle(unsigned int size)
 
     for (int o = 1; o <= size; o++)
     {
-        for (int i = 0; i < size - o; i++, printf("%c", dot))
-            ;
-        for (int n = 0; n < o; n++, printf("%c", sharp))
-            ;
+        for (int i = 0; i < size - o; i++)
+        {
+            printf("%c", dot);
+        };
+
+        for (int n = 0; n < o; n++)
+        {
+            printf("%c", sharp);
+        };
         printf("\n");
     }
 }
