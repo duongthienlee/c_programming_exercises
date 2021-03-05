c3c8c0ee5b71360e9c458409f34db101d9d9b8bb
Thien Ly <duongthienlee@gmail.com>
2021-03-05 17:16:36 +0200

    Finish M06 module exercises
    This finishes M06 6.5 queue exercise
    
    Signed-off-by: Thien Ly <duongthienlee@gmail.com>

--- M06/queue/src/main.c
+++ M06/queue/src/main.c
@@ -5,23 +5,35 @@
 
 int main(void)
 {
-    struct studentqueue q = { NULL, NULL };
+    struct studentqueue q = {NULL, NULL};
 
     int go_on = 1;
     char buffer[100];
-    while(go_on) {
+    while (go_on)
+    {
         printf("Enter name of the student (\"-\" will end reading): ");
         scanf("%99s", buffer);
         buffer[99] = 0;
-        if (strlen(buffer) > 0 && strcmp(buffer, "-")) {
+        if (strlen(buffer) > 0 && strcmp(buffer, "-"))
+        {
             go_on = enqueue(&q, buffer);
-        } else {
+        }
+        else
+        {
             go_on = 0;
         }
     }
 
-    while(dequeue(&q, buffer, 100)) {
-	    printf("Fetched %s from queue\n", buffer);
+    while (dequeue(&q, buffer, 100))
+    {
+        printf("Fetched %s from queue\n", buffer);
+    }
+
+    struct studentqueue *current = &q;
+    while (current)
+    { // Loop until a NULL pointer is reached
+        printf("name: %s\n", current->name);
+        current = current->next;
     }
 
     return 0;
--- M06/queue/src/queue.c
+++ M06/queue/src/queue.c
@@ -1,12 +1,79 @@
 #include "queue.h"
+#include <stdlib.h> // for malloc
+#include <string.h>
+#include <stdio.h>
 
-
+// Create an empty queue
+struct studentqueue *createQueue()
+{
+    struct studentqueue *q = (struct studentqueue *)malloc(sizeof(struct studentqueue));
+    if (q == NULL)
+    {
+        printf("Unable to allocate memory.");
+        free(q);
+    }
+    q->name = NULL;
+    q->next = NULL;
+    return q;
+}
+/* 
+ *  (a): Function int enqueue(struct studentqueue *q, const char *name) that adds given name as the last item of
+ *  the list (that has NULL name in the beginning). The function makes two memory allocations: one for the data
+ *  structure, and other for the name. The function returns value 1, if addition was successful, and value 0
+ *  if it failed (for example because of failed memory allocation).
+ */
 int enqueue(struct studentqueue *q, const char *name)
 {
-    
+    struct studentqueue *new = createQueue();
+    new->name = malloc(strlen(name) + 1);
+    if (new->name == NULL)
+    {
+        free(new);
+        return 0;
+    }
+    strcpy(new->name, name); // allocated name
+    new->next = NULL;        // set next pointer to NULL
+
+    struct studentqueue *current = q;
+    while (current)
+    {
+        if (current->next == NULL)
+        {
+            current->next = new;
+            if (current->next == NULL)
+            {
+                free(current);
+                return 0;
+            }
+            return 1;
+            break;
+        }
+
+        current = current->next;
+    }
+
+    return 1;
 }
 
+/* 
+ *  (b): Function int dequeue(struct studentqueue *q, char *buffer, unsigned int size) that removes the first item from the queue. 
+ *  Before removing the item, the name it contains should be copied to address buffer. 
+ *  In this address there is space for size characters, so you can copy at most size-1 characters. 
+ *  You should also release the memory allocated for this item. The function returns value 1, 
+ *  if there was an item removed from the list, and the related name is copied to buffer. 
+ *  The function returns 0, if there was nothing removed from the list, for example because the list was already empty.
+ */
 int dequeue(struct studentqueue *q, char *buffer, unsigned int size)
 {
-    
+    if (q->next == NULL) // last node, nothing else to delete
+        return 0;
+
+    struct studentqueue *temp;
+    temp = q->next;
+    strncpy(buffer, temp->name, size);
+    q->next = temp->next;
+    free(temp->name);
+    free(temp);
+
+    return 1;
 }
